cb29e544bf00ba046bfaed5c146795886a44a45a
Papoel <pascal.briffard@gmail.com>
2022-06-01 18:30:52 +0200

    Création de la base de données

---
+++ src/Entity/Person.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Common\Filter\SearchFilterInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use App\Repository\PersonRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PersonRepository::class)]
+#[ApiResource(
+    collectionOperations: ['get', 'post'],
+    itemOperations: ['get', 'delete', 'put'],
+    attributes: [
+        'pagination_enabled' => false,
+    ],
+)]
+#[ApiFilter(
+    SearchFilter::class, properties: [
+        'firstname'=> 'partial',
+        'lastname'=> 'partial',
+    ]
+)]
+#[ApiFilter(
+    OrderFilter::class,
+    properties: [
+        'firstname' => SearchFilter::STRATEGY_PARTIAL,
+        'lastname' => SearchFilter::STRATEGY_PARTIAL,
+    ],
+    arguments: ['orderParameterName' => 'order']
+)]
+class Person
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $firstname;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $lastname;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+}
