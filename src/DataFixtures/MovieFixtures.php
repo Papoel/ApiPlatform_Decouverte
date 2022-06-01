e3dcebb020bdcef690b4bed85791f6a5ac2412a8
Papoel <pascal.briffard@gmail.com>
2022-06-01 20:14:38 +0200

    Correction erreurs

--- src/DataFixtures/MovieFixtures.php
+++ src/DataFixtures/MovieFixtures.php
@@ -6,35 +6,36 @@ use App\Entity\Genre;
 use App\Entity\Movie;
 use App\Entity\Person;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class MovieFixtures extends Fixture
+class MovieFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
         /** @var array<array-key, Genre > $genres */
-        $genres= $manager->getRepository(Genre::class)->findAll();
+        $genres = $manager->getRepository(Genre::class)->findAll();
 
-        /** @var array<array-key, Person > $people */
-        $people= $manager->getRepository(Person::class)->findAll();
+        /** @var array<array-key, Person > $persons */
+        $persons = $manager->getRepository(Person::class)->findAll();
 
         foreach ($genres as $genre) {
             for ($i = 1; $i <= 10; $i++) {
                 $movie = (new Movie())
-                    ->setTitle(sprintf('Titre %d', $i))
-                    ->setSynopsis(sprintf('Synopsis %d', $i))
-                    ->setDuration(rand(80, 300))
-                    ->setProductionYear(rand(1930, 2022))
-                    ->setGenre($genre);
+                        ->setTitle(sprintf('Titre %d', $i))
+                        ->setSynopsis(sprintf('Synopsis %d', $i))
+                        ->setDuration(rand(80, 300))
+                        ->setProductionYear(rand(1930, 2022))
+                        ->setGenre($genre);
 
-                shuffle($people);
+                shuffle($persons);
 
-                foreach (array_slice($people, 3, 3) as $person) {
-                    $movie->getActors()->addActor($person);
+                foreach (array_slice($persons, 0, 3) as $person) {
+                    $movie->getActors()->add($person);
                 }
 
-                foreach (array_slice($people, 3, 2) as $person) {
-                    $movie->getDirectors()->addDirector($person);
+                foreach (array_slice($persons, 3, 2) as $person) {
+                    $movie->getDirectors()->add($person);
                 }
 
 
@@ -44,5 +45,13 @@ class MovieFixtures extends Fixture
 
 
         $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            GenreFixtures::class,
+            PersonFixtures::class,
+        ];
     }
 }
